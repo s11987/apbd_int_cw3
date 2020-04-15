a539b4e9d6eed81beb6e0ecef178cafd5fa8f6ae
Karol Galazka <ugryzionybanan@gmail.com>
2020-04-15 12:14:07 +0200

    zadanie 1,2,3

---
+++ apbd_int_cw3/apbd_int_cw3/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_int_cw3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string getStudent()
+        {
+            return "Jan Kowalski";
+        }
+    }
+}
