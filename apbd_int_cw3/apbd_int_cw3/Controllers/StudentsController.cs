903bc37d222706fd9cd213d233e35f5e6f9a15e4
Karol Galazka <ugryzionybanan@gmail.com>
2020-04-15 12:42:59 +0200

    zadanie 6

--- apbd_int_cw3/apbd_int_cw3/Controllers/StudentsController.cs
+++ apbd_int_cw3/apbd_int_cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apbd_int_cw3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,7 +16,13 @@ namespace apbd_int_cw3.Controllers
         public string GetStudent(string orderBy)
         {
             return $"Jurandowski, Galakowski, Zygmuntowski sortowanie = {orderBy}";
+        }
 
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 60000)}";
+            return Ok(student);
         }
     }
 }
