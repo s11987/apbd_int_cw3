ac58426c9a5a68c94364954f02e372b9123a393d
Karol Galazka <ugryzionybanan@gmail.com>
2020-04-15 13:20:03 +0200

    zad 7

--- apbd_int_cw3/apbd_int_cw3/Controllers/StudentsController.cs
+++ apbd_int_cw3/apbd_int_cw3/Controllers/StudentsController.cs
@@ -24,5 +24,17 @@ namespace apbd_int_cw3.Controllers
             student.IndexNumber = $"s{new Random().Next(1, 60000)}";
             return Ok(student);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {           
+            return Ok("Student o " + id + " zostal usuniety");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id, Student student)
+        {            
+            return Ok("student zostal zaktualizowany");
+        }
     }
 }
